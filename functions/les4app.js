f1d46d096b6e0d123fef492afcf8edc8668ef828
LenaKushniruk <114736070+LenaKushniruk@users.noreply.github.com>
2022-12-21 20:45:27 +0100

    Add files via upload

---
+++ functions/les4app.js
@@ -0,0 +1,17 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        let name = "MARIO!!";
+        let say = "BWAHAHA!!!";
+        
+        console.log(name);
+        console.log(say);
+    }
+
+
+}
+
+let app = new App();
+app.runApplication();
